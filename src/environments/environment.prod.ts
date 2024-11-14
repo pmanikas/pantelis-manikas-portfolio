f8edd6e4d15dc00ac3bb72773b3caa8d5a584c37
Pandelis Manikas <pmanikas.social@gmail.com>
2024-11-14 15:16:14 +0200

    Fix typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_ROOT: 'https://api.pantelismanikas.com/',
-  BLOG_API_BASE_URL: 'https://api.gamemedium.com/2/',
+  BLOG_API_BASE_URL: 'https://api.gamemedium.com/2',
 };
