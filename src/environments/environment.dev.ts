4d1604af2d265e9cb6e9d92846696cde9f99d5a5
Pandelis Manikas <pmanikas.social@gmail.com>
2024-11-18 13:20:18 +0200

    Implement automated grid system

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_ROOT: 'http://localhost:3100/',
+  API_ROOT: 'https://api.pantelismanikas.com/',
   BLOG_API_BASE_URL: 'https://devapi.gamemedium.com/v2',
 };
