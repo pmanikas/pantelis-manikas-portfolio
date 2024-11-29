6027d28764934a5316676b776dd344df75525385
Pandelis Manikas <pmanikas.social@gmail.com>
2024-11-29 15:17:13 +0200

    Filter projects by type

--- src/app/portfolio/components/projects/projects.component.ts
+++ src/app/portfolio/components/projects/projects.component.ts
@@ -1,15 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Project } from 'src/app/shared/models/project.model';
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+    selector: 'app-projects',
+    templateUrl: './projects.component.html',
+    styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnChanges {
 
-  @Input() projects: Project[] = [];
+    @Input() projects: Project[] = [];
+    @Input() shouldFilterByType: boolean = false;
 
-  constructor() { }
+    public filteredProjects: Project[] = [];
+    public types: Set<string> = new Set();
+    public selectedType: string = '';
 
+    constructor() { }
+
+    ngOnChanges(): void {
+        if (this.projects.length > 0) {
+            this.grabTypes();
+            this.filterByType('');
+        }
+    }
+
+    private grabTypes(): void {
+        this.projects.forEach(project => {
+            this.types.add(project.type);
+        });
+    }
+
+    public filterByType(type: string): void {
+        this.selectedType = type;
+
+        if(!type || !this.shouldFilterByType) {
+            this.filteredProjects = this.projects;
+            return;
+        }
+
+        this.filteredProjects = this.projects.filter(project => project.type === type);
+    }
 }
